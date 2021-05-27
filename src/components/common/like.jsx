92cf70a2680a3143415a9380dae1e09acc122d4e
Manish Rawal <rawalsinghmanish@gmail.com>
2021-05-27 15:56:31 +0545

    deleting-this-reference-after-converting-to-stateless-function-component

--- src/components/common/like.jsx
+++ src/components/common/like.jsx
@@ -2,10 +2,10 @@ import React, { Component } from "react";
 
 const Like = () => {
   let classes = "fa fa-heart";
-  if (!this.props.liked) classes += "-o";
+  if (!props.liked) classes += "-o";
   return (
     <i
-      onClick={this.props.onToggle}
+      onClick={props.onToggle}
       style={{ cursor: "pointer" }}
       className={classes}
       aria-hidden="true"
