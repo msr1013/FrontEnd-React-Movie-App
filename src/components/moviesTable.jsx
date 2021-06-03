ddda4b587895336bc43eb9aa51e81c22e9a9cd2f
Manish Rawal <rawalsinghmanish@gmail.com>
2021-06-03 10:43:53 +0545

    extracting-tableHeader-tableBody-components-from-movieTable-component-to-new-table-component

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,7 +1,7 @@
+import { Tab } from "bootstrap";
 import React, { Component } from "react";
 import Like from "./common/like";
-import TableBody from "./common/tableBody";
-import TableHeader from "./common/tableHeader";
+import Table from "./common/table";
 
 class MoviesTable extends Component {
   columns = [
@@ -31,14 +31,12 @@ class MoviesTable extends Component {
     const { movies, sortColumn, onSort } = this.props;
 
     return (
-      <table className="table">
-        <TableHeader
-          columns={this.columns}
-          sortColumn={sortColumn}
-          onSort={onSort}
-        />
-        <TableBody data={movies} columns={this.columns} />
-      </table>
+      <Table
+        columns={this.columns}
+        data={movies}
+        sortColumn={sortColumn}
+        onSort={onSort}
+      />
     );
   }
 }
